5d66c827f336c06e4bcaeca52eb6a6552f221c4a
Josef Habich <johabich@edu.aau.at>
2018-03-07 19:40:55 +0100

    UI aufgebaut, ansprechen der Buttons und TextFields

--- app/src/main/java/com/example/jopihabich/einzelabgabev1/MainActivity.java
+++ app/src/main/java/com/example/jopihabich/einzelabgabev1/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.jopihabich.einzelabgabev1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+        public void onClick(View v) {
+            editText = (EditText) findViewById(R.id.editText);
+            textView = (TextView) findViewById(R.id.textView3);
+            textView.setText(editText.getText().toString());
+
+
+        }
+    });
+
+}}
