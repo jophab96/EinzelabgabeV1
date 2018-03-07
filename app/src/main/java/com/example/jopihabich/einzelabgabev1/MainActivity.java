416dbdb94dbcf4b9ee477635896e1449acb99f2e
Josef Habich <johabich@edu.aau.at>
2018-03-07 19:54:36 +0100

    Palindrom Check Funktion eingebunden

--- app/src/main/java/com/example/jopihabich/einzelabgabev1/MainActivity.java
+++ app/src/main/java/com/example/jopihabich/einzelabgabev1/MainActivity.java
@@ -19,17 +19,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
-        public void onClick(View v) {
-            editText = (EditText) findViewById(R.id.editText);
-            textView = (TextView) findViewById(R.id.textView3);
-            textView.setText(editText.getText().toString());
+            public void onClick(View v) {
+                editText = (EditText) findViewById(R.id.editText);
+                textView = (TextView) findViewById(R.id.textView3);
+                textView.setText(editText.getText().toString());
 
 
+            }
+        });
+
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    public String genMessage() {
+
+        if(editText.getText().toString().length()==0 | editText.getText().toString() ==null){
+            return "No Input";
+        }else if(editText.getText().toString().length()<5){
+            return "Input is to short";
         }
-    });
 
-}}
+        if (isPalindrome(editText.getText().toString())) {
+            return "It is a Palindrome!";
+        }else {
+            return "No Palindrome!";
+        }
+    }
+}
